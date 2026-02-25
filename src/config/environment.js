634f671c77029931ae199483f7311b52b390f11e
nguyenan12 <nguyenann1204@gmail.com>
2026-02-25 15:40:12 +0700

    add package and features for project

--- src/config/environment.js
+++ src/config/environment.js
@@ -6,5 +6,7 @@ export const env = {
   LOCAL_DEV_APP_HOST: process.env.LOCAL_DEV_APP_HOST,
   LOCAL_DEV_APP_PORT: process.env.LOCAL_DEV_APP_PORT,
 
-  AUTHOR: process.env.AUTHOR
+  AUTHOR: process.env.AUTHOR,
+
+  BUILD_MODE: process.env.BUILD_MODE
 }
--- src/server.js
+++ src/server.js
@@ -5,9 +5,20 @@ import express from 'express'
 import exitHook from 'async-exit-hook'
 import { GET_DB, CONNECT_DB, CLOSE_DB } from './config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from './routes/v1'
+import cors from 'cors'
+import { corsOptions } from './config/cors'
 
 const START_SERVER = () => {
   const app = express()
+
+  app.use(express.json())
+
+  //xu ly cors
+  app.use(cors(corsOptions))
+
+  //use APIs in v1
+  app.use('/v1', APIs_V1)
 
   app.listen(env.LOCAL_DEV_APP_PORT, env.LOCAL_DEV_APP_HOST, () => {
     console.log(`Hello Trung Quan Dev, I am running at ${env.LOCAL_DEV_APP_PORT}:${env.LOCAL_DEV_APP_HOST}/`)
